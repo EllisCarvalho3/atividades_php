a825363bb8abb5267b69da4758e74bd3ec3b6a25
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2025-05-07 22:47:21 -0300

    .

--- atividade-2/questao1/index.php
+++ atividade-2/questao1/index.php
@@ -3,8 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="styles2.css">
+    <link rel="shortcut icon" href="imagens/e_amarelo.png" type="image/x-icon">
     <title>Questão 1</title>
 </head>
+<header>
+<div class="logo-img"> 
+            <img src="images/logo.png" alt="logo">
+        </div>
+    <nav> 
+      <ul>
+        <li><a href="#">Introdução</a></li>
+        <li><a href="#">Sintaxe</a></li>
+        <li><a href="#">Operadores</a></li>
+        <li><a href="#">Arrays</a></li>
+        <li><a href="#">Depuração</a></li>
+        <li><a href="#">API'S</a></li>
+      </ul>
+    </nav>
+  </header>
 <body>
 
     <?php
