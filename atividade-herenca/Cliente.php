ef6037b78a7139c47f735895185a79510032043a
EllisCarvalho3 <146475582+EllisCarvalho3@users.noreply.github.com>
2025-05-20 12:12:01 -0300

    .

---
+++ atividade-herenca/Cliente.php
@@ -0,0 +1,57 @@
+<!-- criar uma classe usando extends para cliente fisico e juridico, ou seja, a class cliente possui atributos universais (endereço, bairro e verendereço), enquanto que o fisico (tem cpf, nome e o atributo para ver a informação do usuário) e juridico (tem cnpj, ver nome fantasia e ver a informação da empresa) possuem atributos únicos -->
+
+<?php
+class Cliente {
+    public string $endereco;
+    public string $bairro;
+
+    public function __construct($endereco, $bairro)
+    {
+        $this->endereco = $endereco;
+        $this->bairro = $bairro;
+    }
+
+    public function verEndereco()
+    {
+        return "Endereço: " . $this->endereco . ", Bairro: " . $this->bairro;
+    }
+}
+
+class ClienteFisico extends Cliente
+{
+    public string $cpf;
+    public string $nome;
+
+    public function __construct($cpf, $nome, $endereco, $bairro)
+    {
+        parent::__construct($endereco, $bairro);
+        $this->cpf = $cpf;
+        $this->nome = $nome;
+    }
+
+    public function verInformacao()
+    {
+        return "Nome: " . $this->nome . ", CPF: " . $this->cpf . ", " . $this->verEndereco();
+    }
+}
+
+class ClienteJuridico extends Cliente
+{
+    public string $cnpj;
+    public string $nomeFantasia;
+
+    public function __construct($cnpj, $nomeFantasia, $endereco, $bairro)
+    {
+        parent::__construct($endereco, $bairro);
+        $this->cnpj = $cnpj;
+        $this->nomeFantasia = $nomeFantasia;
+    }
+
+    public function verInformacao()
+    {
+        return "Nome Fantasia: " . $this->nomeFantasia . ", CNPJ: " . $this->cnpj . ", " . $this->verEndereco();
+    }
+}
+
+
+?>
---
+++ atividade-herenca/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../styles1.css?v=1">
+    <link rel="shortcut icon" href="../imagens/e_amarelo.png" type="image/x-icon">
+    <title>Desafio</title>
+</head>
+<header>
+<div class="logo-img"> 
+            <img src="../imagens/logo.png" alt="logo">
+        </div>
+    <nav> 
+      <ul>
+        <li><a href="#">Introdução</a></li>
+        <li><a href="#">Sintaxe</a></li>
+        <li><a href="#">Operadores</a></li>
+        <li><a href="#">Arrays</a></li>
+        <li><a href="#">Depuração</a></li>
+        <li><a href="#">API'S</a></li>
+      </ul>
+    </nav>
+  </header>
+<br><br><br><br><br>
+<body>
+    <main>
+        <div class="heranca">
+            <h1>Herença em POO.</h1>
+            <?php
+                require 'Cliente.php';
+                $clienteFisico = new ClienteFisico("12345678900", "Webberzinho de Souza Fantini", "Rua das Panquecas, 123", "Centro");
+                $msg = $clienteFisico->verInformacao();
+                echo "<h3>$msg</h3>";
+
+                $clienteJuridico = new ClienteJuridico("12345678000195", "Fantini Panquecas e Cia", "Avenida das Panquecas, 456", "Centro");
+                $msg = $clienteJuridico->verInformacao();
+                echo "<h3>$msg</h3>";
+            ?>
+        </div>
+    </main>
+
+<br><br><br>
+
+<footer>
+        <span class="footer-title">Ellis Carvalho Xavier</span>
+        <ul class="socials">
+            <li><a href="https://wa.me/qr/KNIVFKKZMUUXH1"><i class="bi bi-whatsapp"></i></a></li>
+            <li><a href="https://pt-br.facebook.com/"><i class="bi bi-facebook"></i></a></li>
+            <li><a href="https://x.com/"><i class="bi bi-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/elliscarv/"><i class="bi bi-instagram"></i></a></li>
+        </ul>
+        <div class="info">
+            <ul>
+                <li class="footer-list_header">Oferece</li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="contact.php">Contato</a></li>
+            </ul>
+    
+            <ul>
+                <li class="footer-list_header">Documentos</li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Políticas de Privacidade</a></li>
+                <li><a href="#">Termos de Serviço</a></li>
+                <li><a href="#">Cookies</a></li>
+            </ul>
+    
+            <ul>
+                <li class="footer-list_header">Para você</li>
+                <li><a href="#">Manuais</a></li>
+                <li><a href="#">Tutoriais</a></li>
+                <li><a href="#">Dicas e Truques</a></li>
+                <li><a href="#">F&Q</a></li>
+            </ul>
+    
+            <ul>
+                <li class="footer-list_header">Trabalhe conosco</li>
+                <li><a href="#">Afiliar</a></li>
+                <li><a href="#">Colaborações</a></li>
+                <li><a href="#">Patrocinadores</a></li>
+                <li><a href="#">Parcerias</a></li>
+        </div>
+        <p>Copyright &copy 2025 Ellis Carvalho Xavier. Todos os diereitos reservados. </p>
+    </footer>
+</body>
+</html>
+
+
+
